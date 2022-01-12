938881f8724ccd39a5ae264ee07a5b1b4cf4b152
himanshiagrawal1403 <94111335+himanshiagrawal1403@users.noreply.github.com>
2022-01-12 23:15:23 +0530

    Add files via upload

---
+++ PrintElements3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PrintElements3 {
+    static int arr[];
+
+    //class Elements1 {
+
+        public static void main(String[] args) {
+            arr = new int[5];
+            Scanner sc = new Scanner(System.in);
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println(arr[i]);
+
+            }
+
+        }
+    }
